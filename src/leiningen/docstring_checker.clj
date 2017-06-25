dd70e8162e19b00ec5b50d5b635f93ad433d4c65
Jon Bristow <jon.bristow@teradata.com>
2017-06-24 21:05:08 -0700

    Adding defaults

--- src/leiningen/docstring_checker.clj
+++ src/leiningen/docstring_checker.clj
@@ -1,11 +1,18 @@
 (ns leiningen.docstring-checker
-  (:require [leiningen.core.eval :as lein]))
+  (:require [leiningen.core.eval :as lein]
+            [leiningen.compile :as lein-compile]
+            [bultitude.core :as b]))
 
 (defn- docstring-checker-version [{:keys [plugins]}]
   (some (fn [[plugin-name version]]
           (when (= plugin-name 'docstring-checker/docstring-checker)
             version))
         plugins))
+
+(defn get-packages [paths]
+  (map #(re-pattern (str "^" (name %)))
+       (b/namespaces-on-classpath :classpath
+                                  (map clojure.java.io/file paths))))
 
 (defn docstring-checker
   "Linter that checks all public vars in a Leiningen project have docstrings."
@@ -15,4 +22,7 @@
    (update-in project [:dependencies] conj ['docstring-checker (docstring-checker-version project)])
    `(do
       (require 'docstring-checker.core)
-      (docstring-checker.core/check-docstrings ~(:docstring-checker project)))))
+      (if ~(:docstring-checker project)
+        (docstring-checker.core/check-docstrings ~(:docstring-checker project))
+        (docstring-checker.core/check-docstrings {:include (vector ~@(get-packages (:source-paths project)))
+                                                  :exclude []})))))
