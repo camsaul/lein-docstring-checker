2a7e3407714b85b405666d004f6f164e914e8156
Ryan Senior <senior.ryan@gmail.com>
2018-11-07 09:16:00 -0600

    Ignore proxy classes
    When using `proxy` in Clojure, it will create a new public var that is
    not a `def` that doesn't have docs without an easy way to add it. It
    would make more sense to doc the `def`/`defn` form that creates or
    binds the `proxy` anyway. This commit adds a function to determine if
    the var is a proxy and ignores it.

--- test-projects/good/src/good/core.clj
+++ test-projects/good/src/good/core.clj
@@ -4,3 +4,10 @@
   "I'm ok, because I have a docstring."
   [x]
   (println x "Hello, World!"))
+
+(defn create-a-proxy
+  "A docstring here"
+  []
+  ;; This generates a proxy class which is a public var but should be ignored
+  (proxy [java.lang.Object] []
+    (toString [] "I'm an object")))
