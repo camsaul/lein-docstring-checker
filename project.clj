6b476ee8b392723091d8b7920d4076450f613df1
Cam Saul <github@camsaul.com>
2019-11-05 14:20:10 -0800

    Update CircleCI config

--- project.clj
+++ project.clj
@@ -1,8 +1,19 @@
 (defproject docstring-checker "1.1.0"
-  :description         "Linter that checks that public vars in your project all have docstrings."
-  :url                 "https://github.com/camsaul/lein-docstring-checker"
-  :license             {:name "3-Clause BSD"
-                        :url  "https://github.com/camsaul/lein-docstring-checker/blob/master/LICENSE.txt"}
-  :eval-in-leiningen   true
-  :dependencies        [[org.clojure/tools.namespace "0.2.11"]]
-  :deploy-repositories [["clojars" {:sign-releases false}]])
+  :description "Linter that checks that public vars in your project all have docstrings."
+  :url         "https://github.com/camsaul/lein-docstring-checker"
+
+  :license
+  {:name "3-Clause BSD"
+   :url  "https://github.com/camsaul/lein-docstring-checker/blob/master/LICENSE.txt"}
+
+  :eval-in-leiningen true
+
+  :dependencies
+  [[org.clojure/tools.namespace "0.2.11"]]
+
+  :deploy-repositories
+  [["clojars"
+    {:url           "https://clojars.org/repo"
+     :username      :env/clojars_username
+     :password      :env/clojars_password
+     :sign-releases false}]])
